01d98d195637e219975b3863696f628b866f85ca
xczpw <xg110zpw@163.com>
2017-07-17 12:58:28 +0800

    Update GRID.js

--- GRID.js
+++ GRID.js
@@ -2,20 +2,32 @@ function GRID(gridID){
 	var grid = $("#"+gridID);
 	grid.info=grid.getInfo();	
 	grid.row_num=grid.getNumberRows();
-	grid.col_num=grid.find(".row.pmdynaform-grid-thead").find("div").not(".wildcard").not(".pmdynaform-grid-removerow-static").not(":hidden").length;
+	grid.col_vis=getVisiableColumnIndex();
+	//grid.col_num=grid.find(".row.pmdynaform-grid-thead").find("div").not(".wildcard").not(".pmdynaform-grid-removerow-static").not(":hidden").length;
 	
-	grid.getData=function(){
+	function getVisiableColumnIndex(){
+		var cols=grid.find(".row.pmdynaform-grid-thead").find("div").not(".wildcard").not(".pmdynaform-grid-removerow-static");
+		var col_vis_index=[];
+		for(var i=1;i<=cols.length;i++){
+			if($(cols[i-1]).is(":visible")){
+				col_vis_index.push(i);
+			}
+		}
+		return col_vis_index;
+	}
+	
+	grid.getData=function(){ //[Array(),Array(),Array()....]
 		var data=[];
 		var label=[];
-		for(var i=1;i<=this.col_num;i++){
-			label.push(this.getLabel(i));
-		}
+		$.each(this.col_vis, function(index,colIndex){
+			label.push(grid.getLabel(colIndex));
+		});
 		data.push(label); //标题行
 		for(var i=1;i<=this.row_num;i++){
 			var row_data=[];
-			for(var j=1;j<=this.col_num;j++){
-				row_data.push(this.getValue(i,j).replace(/(^\s*)|(\s*$)/g,""));
-			}
+			$.each(this.col_vis, function(index,colIndex){
+				row_data.push(grid.getValue(i,colIndex).replace(/(^\s*)|(\s*$)/g,""));
+			});
 			data.push(row_data);
 		}
 		return(data);	
@@ -25,7 +37,7 @@ function GRID(gridID){
 		var rn=this.row_num;
 		if(needEmpty){ //只删除空行
 			while(rn>0){
-				if(this.isEmptyRow(rn)){
+				if(isEmptyRow(rn)){
 					grid.deleteRow(rn);
 				}
 				rn--;
@@ -38,27 +50,25 @@ function GRID(gridID){
 		}
 	}
 	
-	grid.isEmptyRow=function(rowID){
-		var cn = this.col_num;
-		for(var i=1;i<=cn;i++){
-			if(this.getValue(rowID,i)!=="")
-				return false;
-		}
+	function isEmptyRow(rowID){
+		$.each(this.col_vis, function(index,colIndex){
+			if(this.getValue(rowID,colIndex)!=="") return false;
+		});
 		return true;
 	}
 	
 	grid.setOnchange=function(fun){
-		var rn=this.row_num,cn=this.col_num;
+		var rn=this.row_num,cn=this.col_vis.length;
 		for(var r=1;r<=rn;r++){
-			for(var c=1;c<=cn;c++){
-				var gc=this.getControl(r, c);
-				gc.attr({'row':r,'column':c});
+			$.each(this.col_vis, function(index,colIndex){
+				var gc=grid.getControl(r, colIndex);
+				gc.attr({'row':r,'column':colIndex});
 				gc.change(function(){
 					var row=$(this).attr('row'),col=$(this).attr('column');
 					var val=$(this).val();
 					fun(row,col,val);
 				});
-			}	
+			});
 		}	
 	}
 	
@@ -68,7 +78,6 @@ function GRID(gridID){
 		}else{ //开启监听
 			this.onAddRow(function(r, g, index){
 				fun(r,g,index);
-				this.row_num++;
 			});
 		}
 	}
@@ -82,9 +91,14 @@ function GRID(gridID){
 			});
 		}
 	}
-	
-	grid.onAddRow(function(){this.row_num++;});
-	grid.onDeleteRow(function(){this.row_num--;});
+
+	grid.refresh=function(fun_c,fun_a,fun_d){ //建议不单独使用各个事件，而直接使用该函数
+		this.row_num=this.getNumberRows();
+		this.col_vis=getVisiableColumnIndex();
+		this.setOnchange(fun_c);
+		this.setOnAddRow(fun_a);
+		this.setOnDeleteRow(fun_d);
+	}
 	
 	return grid;
 }
